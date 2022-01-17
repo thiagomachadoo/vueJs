58bd6d8a950b18c2c27ab2edf616ee00e8ae9e39
Thiago Machado <thiagodasilvamachadoo44@gmail.com>
2022-01-17 10:52:22 -0300

    terminando o desafio e entedendo a diretiva v-bind

--- dom/desafio/app.js
+++ dom/desafio/app.js
@@ -3,8 +3,16 @@ new Vue({
     el: "#desafio",
     data: {
         name: "Thiago",
-        birthday: "today",
-        years: 20 * 3
-    }
+        idade: 15,
+        image: "http://files.cod3r.com.br/curso-vue/vue.jpg"
+    },
+    methods: {
+      idadePor3(){
+         return this.idade * 3
+      },
+      random(){
+         return Math.random()
+      }  
+    },
 })
 
